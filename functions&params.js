604e8ebf00620b12a789d4c6b8ea3eb3a2c4f3d6
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-21 16:59:22 -0500

    Update functions&params.js

--- functions&params.js
+++ functions&params.js
@@ -1,2 +1,30 @@
 const fname = 'justin';
+const youtuber = 'youtuber';
 
+///older
+function toUpper(text) {
+    const upperCase = text.toUpper();
+    console.log(upperCase);
+    
+
+}
+
+toUpper (fname);
+toUpper(youtuber);
+
+
+function adder (num1, num2) { 
+    console.log(num1 + num2);
+}
+adder(10, 5);
+
+///modern
+const toUpperCase = function (){
+    asasdff
+}
+
+
+///es7
+const adder =() => {
+    asafd
+}
