61dc22f6e80a94f89808e7b636e642f4310b7cb8
Emanuel Joness <itzemanuelj@gmail.com>
2022-12-21 16:39:35 -0500

    added app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log('Hello World');
+
+var a;
+let b;
+const c = 1;
+
+var life = 100;
+
+let fname = 'Justin';
+
+/// adding notes
+
+console.log(life);
+console.log(fname);
+
+life = 50;
+
+life = life - 10;
+
+console.log(life);
+
+var checkout = false;
+
+if (checkout === true) {
+    console.log('we can checkout');
+}
+else {
+    console.log(`we can't checkout`);
+}
+
+///string
+///booleen
+///null 
+///undefined
+///number
+///object
+///symbol
+
+let life = 100;
